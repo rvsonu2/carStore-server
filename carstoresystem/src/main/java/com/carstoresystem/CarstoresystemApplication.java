4a0498ab962fcd3b5f4a32e0d13da49e399519f0
RAVI RANJAN <rvsonu2@gmail.com>
2023-06-10 19:09:27 +0530

    build structure

---
+++ carstoresystem/src/main/java/com/carstoresystem/CarstoresystemApplication.java
@@ -0,0 +1,13 @@
+package com.carstoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstoresystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarstoresystemApplication.class, args);
+	}
+
+}
